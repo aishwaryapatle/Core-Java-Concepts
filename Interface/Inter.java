b638e5590dd1f5e888fb1f329b3c6ffb1a22c116
Aishwarya Patle <86393603+aishwaryapatle@users.noreply.github.com>
2022-09-22 12:54:37 +0530

    Create Inter.java

---
+++ Interface/Inter.java
@@ -0,0 +1,18 @@
+interface Bank
+{
+	void setCustDetails();
+	abstract void showCusDetails();
+	public static void main(String[] args) 
+	{
+		Bank.myfun();
+	}
+	static void myfun()
+	{
+		System.out.println("hello myfun");
+	}
+}
+public class Inter {
+
+	
+
+}
